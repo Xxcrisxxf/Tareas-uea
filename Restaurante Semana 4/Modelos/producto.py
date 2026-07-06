541d9965baaa19a9648da23fcb0f8850b46c2e8d
Xxcrisxxf <bravocristian730@gmail.com>
2026-07-05 22:52:18 -0500

    Delete Restaurante Semana 4 directory

--- Restaurante Semana 4/Modelos/producto.py
+++
@@ -1,9 +0,0 @@
-class Producto:
-    #Constructor para los productos
-    def __init__(self , nombre , precio, bebida):
-        self.nombre = nombre
-        self.precio = precio
-        self.bebida = bebida
-    #Creamos un metodo  especial para representar la clase como texto
-    def __str__(self):
-        return f"Producto : {self.nombre} / Precio : {self.precio} / Bebida : {self.bebida}"
